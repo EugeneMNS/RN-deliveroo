456f9b3ccb1f30bcbd0a5b8b0296a95788aa2025
EugeneMNS <evgen.str4@gmail.com>
2022-11-14 18:46:13 +0300

    feat: header & search & body

---
+++ components/Categories.js
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native";
+import React, {useState, useEffect} from "react";
+
+const Categories = () => {
+    return (
+        <View>
+            <Text>Categories</Text>
+        </View>
+    );
+}
+
+export default Categories;
